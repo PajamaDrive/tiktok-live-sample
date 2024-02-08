2e0f8031a3763b8a34df270d9e37d99169fb516d
PajamaDrive <pajamadrive49@gmail.com>
2024-02-08 23:53:48 +0900

    [remove] 不要なコードなどを削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { initIo } from './src/server/lib/realtime';
-import { type ViteDevServer } from 'vite';
-
-const webSocketServer = {
-	name: 'webSocketServer',
-	configureServer(server: ViteDevServer) {
-		if (!server.httpServer) return;
-		initIo(server.httpServer);
-	}
-};
 
 export default defineConfig({
-	plugins: [sveltekit(), webSocketServer],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
